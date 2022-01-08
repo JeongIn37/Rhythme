e5b721786eac3693a78a4d6e3654885818d954cf
6590f1 <6590f1@naver.com>
2022-01-08 14:34:48 +0900

    관절각도 인식 추가

---
+++ 2stWeek/Rhythme/app/src/main/java/com/cs496/rhythm/MainActivity.kt
@@ -0,0 +1,142 @@
+package com.cs496.rhythm
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executors
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import com.cs496.rhythm.calcPoseVector.getPoseVector
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.pose.PoseDetection
+import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.ExecutorService
+
+
+class MainActivity : AppCompatActivity(), calcPoseVector {
+
+    private lateinit var cameraExecutor: ExecutorService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.surfaceProvider)
+                }
+
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, YourImageAnalyzer {})
+                }
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageAnalyzer)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private class YourImageAnalyzer(function: (Any?) -> Unit) : ImageAnalysis.Analyzer {
+
+        val options = PoseDetectorOptions.Builder()
+            .setDetectorMode(PoseDetectorOptions.STREAM_MODE)
+            .build()
+        val poseDetector = PoseDetection.getClient(options)
+
+        @SuppressLint("UnsafeOptInUsageError")
+        override fun analyze(imageProxy: ImageProxy) {
+            val mediaImage = imageProxy.image
+            Log.d("Analyzer","이미지변환")
+            if (mediaImage != null) {
+                Log.d("Analyzer","이미지 분석 시도")
+                val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+                poseDetector.process(image)
+                    .addOnSuccessListener { results ->
+                        Log.d("Analyzer","스켈레톤 추출 성공, 관절갯수${results.allPoseLandmarks.size}")
+                        if(results.allPoseLandmarks.size>0){
+                            getPoseVector(results)
+                            Log.d("관절각도",getPoseVector(results).toString())
+                        }
+                    }
+                    .addOnFailureListener { e ->
+                        Log.d("Analyzer","이미지 분석 실패 ${e.toString()}")
+                    }
+                    .addOnCompleteListener{
+                        imageProxy.close()
+                    }
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+}
---
+++ 2stWeek/Rhythme/app/src/main/java/com/cs496/rhythm/calcPoseVector.java
@@ -0,0 +1,36 @@
+package com.cs496.rhythm;
+
+import static java.lang.Math.atan2;
+
+import com.google.mlkit.vision.pose.Pose;
+import com.google.mlkit.vision.pose.PoseLandmark;
+
+public interface calcPoseVector {
+
+    static double getAngle(PoseLandmark first,PoseLandmark midle, PoseLandmark last)
+    {
+
+        double result = Math.toDegrees(atan2(last.getPosition().y - midle.getPosition().y, last.getPosition().x - midle.getPosition().x) - atan2(first.getPosition().y - midle.getPosition().y, first.getPosition().x - midle.getPosition().x));
+
+        result = Math.abs(result);
+        if(result > 180){
+            result = 360 - result;
+        }
+
+        return result;
+    }
+
+    static double[] getPoseVector(Pose pose) {
+        double[] temp = new double[]{0,0,0,0,0,0,0,0};
+        temp[0]=getAngle(pose.getPoseLandmark(16),pose.getPoseLandmark(14),pose.getPoseLandmark(12));
+        temp[1]=getAngle(pose.getPoseLandmark(14),pose.getPoseLandmark(12),pose.getPoseLandmark(24));
+        temp[2]=getAngle(pose.getPoseLandmark(23),pose.getPoseLandmark(11),pose.getPoseLandmark(13));
+        temp[3]=getAngle(pose.getPoseLandmark(11),pose.getPoseLandmark(13),pose.getPoseLandmark(15));
+
+        temp[4]=getAngle(pose.getPoseLandmark(28),pose.getPoseLandmark(26),pose.getPoseLandmark(24));
+        temp[5]=getAngle(pose.getPoseLandmark(26),pose.getPoseLandmark(24),pose.getPoseLandmark(12));
+        temp[6]=getAngle(pose.getPoseLandmark(25),pose.getPoseLandmark(23),pose.getPoseLandmark(11));
+        temp[7]=getAngle(pose.getPoseLandmark(27),pose.getPoseLandmark(25),pose.getPoseLandmark(23));
+        return temp;
+    }
+}
