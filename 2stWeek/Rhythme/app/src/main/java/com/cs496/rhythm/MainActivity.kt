8b66f98ceeeb1277d00f3f12339a18da4143742c
6590f1 <6590f1@naver.com>
2022-01-10 00:31:30 +0900

    게임 플레이 화면(카메라 + 캠) 레이아웃 구현 및 동영상 리스너 구현

--- 2stWeek/Rhythme/app/src/main/java/com/cs496/rhythm/MainActivity.kt
+++ 2stWeek/Rhythme/app/src/main/java/com/cs496/rhythm/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.cs496.rhythm
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.media.MediaMetadataRetriever
+import android.media.MediaPlayer
+import android.net.Uri
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
+import android.widget.VideoView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.util.concurrent.Executors
@@ -14,18 +21,49 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import com.cs496.rhythm.calcPoseVector.getPoseVector
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.pose.PoseDetection
+import com.google.mlkit.vision.pose.accurate.AccuratePoseDetectorOptions
 import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 import java.util.concurrent.ExecutorService
+import kotlin.collections.ArrayList
 
+lateinit var currentPose:Array<Double>
 
 class MainActivity : AppCompatActivity(), calcPoseVector {
 
     private lateinit var cameraExecutor: ExecutorService
+    var timer = Timer()
+    lateinit var videov: VideoView
+    lateinit var posePerTime : ArrayList<DoubleArray>
+    lateinit var tempImage : ArrayList<Bitmap> //담에지울거
+    lateinit var tempViewer : ImageView
+    var time = 0
+    lateinit var retriever : MediaMetadataRetriever
+    lateinit var mediaPlayer : MediaPlayer
+
+    var timerTask: TimerTask = object : TimerTask() {
+        override fun run() {
+            if(posePerTime.size==time){
+                timer.cancel()
+                endGame()
+                return
+            }
+            Log.d("ddd","${tempImage.size}번째 중 ${time}번째")
+            Log.d("현재 카메라 각도","왼쪽 팔꿈치 : ${currentPose[0]}, 왼쪽 어깨 : ${currentPose[1]}, 오른쪽 어깨 : ${currentPose[2]}, 오른쪽 팔꿈치 : ${currentPose[3]}")
+            Log.d("현재 동영상 각도","왼쪽 팔꿈치 : ${posePerTime[time][0]}, 왼쪽 어깨 : ${posePerTime[time][1]}, 오른쪽 어깨 : ${posePerTime[time][2]}, 오른쪽 팔꿈치 : ${posePerTime[time][3]}")
+
+            //currentPose, posePerTime[time]비교
+
+            time++
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        videov = findViewById<VideoView>(R.id.videoView)
 
         // Request camera permissions
         if (allPermissionsGranted()) {
@@ -36,6 +74,65 @@ class MainActivity : AppCompatActivity(), calcPoseVector {
         }
 
         cameraExecutor = Executors.newSingleThreadExecutor()
+
+        loadGame()
+        startGame()
+    }
+
+    fun loadGame() {
+
+        //포즈디텍터 생성
+        val options = AccuratePoseDetectorOptions.Builder()
+            .setDetectorMode(AccuratePoseDetectorOptions.SINGLE_IMAGE_MODE)
+            .build()
+        val poseDetector = PoseDetection.getClient(options)
+
+        //동영상 view 셋팅
+        val videopath = "android.resource://com.cs496.rhythm/" + R.raw.test
+        val uri = Uri.parse(videopath)
+        videov.setVideoURI(uri)
+        retriever = MediaMetadataRetriever()
+        tempImage = ArrayList()
+        posePerTime = ArrayList()
+        retriever.setDataSource(application, uri)
+        mediaPlayer = MediaPlayer.create(baseContext, uri)
+        val millisecond = mediaPlayer.duration
+        Log.d("영상길이",millisecond.toString())
+        for (i in 0 until millisecond / 3000) {
+            val time = ((i * 1000000)*3).toLong()
+            val temp = retriever.getFrameAtTime(time, MediaMetadataRetriever.OPTION_CLOSEST)
+            val image = InputImage.fromBitmap(temp, 0)
+            if (temp != null) {
+                Log.d("dddddd","${time/1000000}초 이미지 등록")
+                tempImage.add(temp)
+            }
+            //이미지 처리 실패시(포즈인식 실패) 앞뒤 영상중 되는 영상 확인
+            Log.d("Analyzer","스켈레톤 추출 시도")
+            poseDetector.process(image)
+                .addOnSuccessListener { result ->
+                    Log.d("Analyzer","스켈레톤 추출 성공, 관절갯수${result.allPoseLandmarks.size}")
+                    if(result.allPoseLandmarks.size>0 && temp!=null){
+                        getPoseVector(result)
+                        val currentPose = getPoseVector(result)
+                        posePerTime.add(currentPose)
+                        Log.d("관절각도","왼쪽 팔꿈치 : ${currentPose[0]}, 왼쪽 어깨 : ${currentPose[1]}, 오른쪽 어깨 : ${currentPose[2]}, 오른쪽 팔꿈치 : ${currentPose[3]}")
+                    }
+                }
+                .addOnFailureListener { e ->
+                    Log.d("Analyzer","이미지 분석 실패 ${e.toString()}")
+                }
+        }
+    }
+
+    fun startGame() {
+        videov.start()
+        timer.schedule(timerTask, 0, 3000)
+    }
+
+    //게임종료후 처리 코
+    fun endGame() {
+        Log.d("게임","끝------------------------")
+        videov.pause()
     }
 
     private fun startCamera() {
@@ -94,8 +191,7 @@ class MainActivity : AppCompatActivity(), calcPoseVector {
                         Log.d("Analyzer","스켈레톤 추출 성공, 관절갯수${results.allPoseLandmarks.size}")
                         if(results.allPoseLandmarks.size>0){
                             getPoseVector(results)
-                            val pose = getPoseVector(results)
-                            Log.d("관절각도","왼쪽 팔꿈치 : ${pose[0]}, 왼쪽 어깨 : ${pose[1]}, 오른쪽 어깨 : ${pose[2]}, 오른쪽 팔꿈치 : ${pose[3]}")
+                            currentPose = getPoseVector(results) as Array<Double>
                         }
                     }
                     .addOnFailureListener { e ->
--- 2stWeek/Rhythme/app/src/main/java/com/cs496/rhythm/SelectVideoActivity.java
+++ 2stWeek/Rhythme/app/src/main/java/com/cs496/rhythm/SelectVideoActivity.java
@@ -10,7 +10,6 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SelectVideoActivity extends AppCompatActivity{
-    @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_list);
